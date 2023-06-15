151dbf5876061e441a122250e3b3fdc29cee3b20
Agustin Urien <agus.urien2@gmail.com>
2023-06-15 15:10:06 -0300

    lo arrgele

--- js/peliculas.js
+++ js/peliculas.js
@@ -6,31 +6,31 @@ const peliculas = [
 ];
 
 const pelicula = document.querySelector(".peliculaFondo");
-const botonPelicula = document.querySelector(".pelicula")
+const botonPelicula = document.querySelectorAll(".pelicula")
 
 
 
+botonPelicula.forEach(boton => {
+    boton.addEventListener("click", (e) => {
+        const peliculaId = e.currentTarget.id;
+        const peliculaSeleccionada = peliculas.find(pelicula => pelicula.codigo == peliculaId)
+        console.log(peliculaId);
+        console.log(peliculaSeleccionada);
 
 
-/*
-botonPelicula.addEventListener("click", (e) => {
-    const peliculaId = e.target.id;
-
-    const peliculaSeleccionada = peliculas.find(pelicula => pelicula.codigo == peliculaId)
-    if (peliculaSeleccionada) {
-        const div = document.createElement("div");
-        div.classList.add("contenidoPelicula");
-        div.innerHTML = `
+        if (peliculaSeleccionada) {
+            const div = document.createElement("div");
+            div.classList.add("contenidoPelicula");
+            div.innerHTML = `
         <h1 class="tituloPeliculas">${peliculaSeleccionada.nombre}</h1>
         <p class="textoPelicula">${peliculaSeleccionada.resumen}</p>
         <img src="${peliculaSeleccionada.img}" alt="${peliculaSeleccionada.nombre}" class="imagenPelicula">
         `;
-
-        pelicula.append(div);
-    }
-
+            pelicula.append(div);
+        }
+    })
 });
-*/
+
 
 
 
